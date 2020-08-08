7d946989ef182de4da9f02d09d61eb8b0108506e
camgrace <69372287+camgrace@users.noreply.github.com>
2020-08-08 16:59:22 -0400

    Update Course.java

--- Course.java
+++ Course.java
@@ -6,6 +6,7 @@ public class Course {
     private boolean needComputer;
     private int tac;            //number of TA
     private int duration;       //in hours (bc counting minutes is a little cursed)
+    private int totalStudentCount;
     //private TreeMap<String, CourseSection> labSections; //to support multiple course sections
                    //Treemap : String (section name? ex. 01, 02) -> CourseSection (course section details)
 
@@ -15,6 +16,7 @@ public class Course {
         needComputer = useComputer;
         tac = numberOfTA;
         duration = labDuration;
+        totalStudentCount = 0; //
     }
 
     public String getCourseCode()
@@ -36,5 +38,25 @@ public class Course {
     {
         return duration;
     }
-
+    
+    /** 
+    * Goes through all student objects and returns number of students in this course.
+    * @param	students    Arraylist of all student objects
+    * @return               Number of students in this course
+    */
+    public int updateStudentCount (ArrayList<Student> students)
+    {
+        int sCount = 0;
+        for (int i = 0; i < students.size(); i++)
+        {
+            Student s = students.get(i);
+            if(s.getCourse1().equals(this.courseCode) || s.getCourse2().equals(this.courseCode) || s.getCourse3().equals(this.courseCode))
+            {
+                sCount+=1;
+            }
+        }
+        return sCount;
+    }
+    
+    
 }
