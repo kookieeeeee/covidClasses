6fe30496f6657df86db208d446dd8d77a3aed5a8
camgrace <69372287+camgrace@users.noreply.github.com>
2020-08-08 17:24:53 -0400

    Update CourseSection.java

--- CourseSection.java
+++ CourseSection.java
@@ -3,34 +3,20 @@ import java.util.ArrayList;
 public class CourseSection extends Course
 {
     int sectionNumber; //could be string ex. Section 1 vs "01"
-    String day; //days (Mon, Fri)
-    int startTime; //hours (0-24)
-    int studentCount;
-    String roomName;
+    int studentCount; //not in constructor
+    String room; //not in constructor
 
-    public CourseSection(String courseCode, boolean needsComputer, int TAC, int labDuration, int sectionNumber, String day, int startTime)
+    public CourseSection(String courseCode, boolean needsComputer, int TAC, String day, int startTime, int endTime, int sectionNumber)
     {
-        super(courseCode, needsComputer, TAC, labDuration); //in Course constructor
+        super(courseCode, needsComputer, TAC, day, startTime, endTime); //in Course constructor
         this.sectionNumber = sectionNumber;
-        this.day = day;
-        this.startTime = startTime;
-        this.studentCount = 0; //looking for this value in updateStudentCount function
-        this.roomName = ""; //looking for this value in findRoom function
+        this.studentCount = 0; //default value
+        this.room = ""; //default value
     }
 
     public int getSectionNumber() //could be string
     {
         return sectionNumber;
-    }
-    
-    public String getDay()
-    {
-        return day;
-    }
-    
-    public int getStartTime()
-    {
-        return this.startTime;
     }
     
     public int getStudentCount()
@@ -42,30 +28,10 @@ public class CourseSection extends Course
     {
         return this.roomName;
     }
-    
-    /** 
-    * Finds the end time of the lab section.
-    * @return    Lab end time (in hours ex. 14 for 2:00 PM)
-    */
-    public int getEndTime ()
-    {
-        return this.getStartTime() + super.getDuration();
-    }
 
-    /** 
-    * Goes through all student objects and returns number of students in this course section.
-    * @param	students    Arraylist of all student objects
-    * @return               Number of students in this course
-    */
-    private int updateStudentCount (ArrayList<Student> students)
+    public String setRoomName(String roomName)
     {
-        int sCount = 0;
-        for (int i = 0; i < students.size(); i++)
-        {
-         // if student is in this course
-            // sCount+=1
-        }
-        return sCount;
+        this.roomName = roomName;
     }
 
     /** 
@@ -73,16 +39,19 @@ public class CourseSection extends Course
     * @param	rooms    Arraylist of all room objects
     * @return            Name of valid classroom for this lab
     */
-    private String findRoom (ArrayList<Classroom> rooms)
+    public String findRoom (ArrayList<Classroom> rooms)
     {
         for (int i = 0; i < rooms.size(); i++)
         {
-            if (rooms.get(i).isCompatible(this))
+            this.studentCount = rooms.get(i).getMaxOccupancy() - this.getNumberOfTA(); //TEMPORARILY set num of students in section as room max occupancy minus TA count
+            if (rooms.get(i).isCompatible(this)) //if it works
             {
                 rooms.get(i).addSection(this);
-                return rooms.get(i).getRoomNumber();
+                super.totalStudentCount-=this.studentCount; //remove students in this course section from course total student count
+                return rooms.get(i).getRoomNumber(); //this room is available
             }
         }
+        this.studentCount = 0; //no suitable room, this course section should not exist
         return "";
     }
 
