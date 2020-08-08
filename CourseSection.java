11e78bec6bdccbc15b5d26131e798cbe579c6237
camgrace <69372287+camgrace@users.noreply.github.com>
2020-08-08 08:02:38 -0400

    Update CourseSection.java

--- CourseSection.java
+++ CourseSection.java
@@ -8,7 +8,7 @@ public class CourseSection extends Course
 
     public class CourseSection(int courseCode, boolean needsComputer, int TAC, int labDuration, int sectionNumber, String day, int startTime)
     {
-        super(courseCode, needsComputer, TAC, labDuration);
+        super(courseCode, needsComputer, TAC, labDuration); //in Course constructor
         this.sectionNumber = sectionNumber
         this.day = day;
         this.startTime = startTime;
@@ -17,37 +17,35 @@ public class CourseSection extends Course
     }
 
     /** 
-	 * Finds the end time of the lab section.
-    * @return           Lab end time (in hours ex. 14 for 2:00 PM)
-	 */
+    * Finds the end time of the lab section.
+    * @return    Lab end time (in hours ex. 14 for 2:00 PM)
+    */
     private int getEndTime ()
     {
         return this.startTime + super.getDuration();
     }
 
     /** 
-	 * Goes through all student objects and returns number of students in this course section.
-	 * @param	students    Arraylist of all student objects
+    * Goes through all student objects and returns number of students in this course section.
+    * @param	students    Arraylist of all student objects
     * @return               Number of students in this course
-	 */
+    */
     private int updateStudentCount (ArrayList<Student> students)
     {
         int sCount = 0;
-
         for (int i = 0; i < students.size(); i++)
         {
          // if student is in this course
             // sCount+=1
         }
-
         return sCount;
     }
 
     /** 
-	 * Goes through all room objects and returns name of valid classroom for this course section.
-	 * @param	rooms    Arraylist of all room objects
+    * Goes through all room objects and returns name of valid classroom for this course section.
+    * @param	rooms    Arraylist of all room objects
     * @return            Name of valid classroom for this lab
-	 */
+    */
     private String findRoom (ArrayList<Classroom> rooms)
     {
         for (int i = 0; i < rooms.size(); i++)
@@ -55,7 +53,6 @@ public class CourseSection extends Course
             //if room suits all course needs (time, materials, availability):
                 // return rooms.getRoomNumber()
         }
-
         return "";
     }
 
