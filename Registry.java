a37bfe4e120ec880707ba0582554fcf8a5a679c1
thaonguyen0229 <47158178+thaonguyen0229@users.noreply.github.com>
2020-08-08 19:09:51 -0400

    update registry

--- Registry.java
+++ Registry.java
@@ -6,17 +6,56 @@ import java.io.IOException;
 
 public class Registry {
     public static void main(String[] args) throws IOException{
-        ArrayList<Student> students = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Course> courses = new ArrayList<Course>();
+        //read courses file
+        try {
+            Scanner scanner = new Scanner(new File("courses.txt"));
+            while (scanner.hasNextLine())
+            {
+                String line = scanner.nextLine();
+                Scanner sc = new Scanner(line);
+                String code = sc.next();
+                boolean needComp = false;
+                if (sc.next().equalsIgnoreCase("yes"))
+                {
+                    needComp = true;
+                }
+                int tac = sc.nextInt();
+                int duration = sc.nextInt();
+                courses.add(new Course(code, needComp, tac, duration));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        //read students file
         try {
             Scanner scanner = new Scanner(new File("studentInfo.txt"));
             while (scanner.hasNextLine())
             {
                 String newline = scanner.nextLine();
+                Scanner sc = new Scanner(newline);
+                String name = sc.next();
+                String id = sc.next();
+                ArrayList<Course> takenCourses = new ArrayList<Course>();
+                for (int i = 1; i <= 3; i++)
+                {
+                    String code = sc.next();
+                    for (Course c: courses)
+                    {
+                        if (code.equalsIgnoreCase(c.getCourseCode())) takenCourses.add(c);
+                    }
+                }
+                students.add(new Student(name, id, takenCourses));
+                /**
                 Student s = registrynew.nextStudent(newline);
                 if(!s.equals(new Student("","", "", "", "")))
                    {
                        students.add(s.getId(), s.getName(), s.getCourse1(), s.getCourse2(), s.getCourse3());
-                   }   
+                   } 
+                   **/  
             }
         } catch (Exception e) {
             System.out.println("studentInfo.txt: "+ "File Not Found Exception");        }
